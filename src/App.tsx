950133ee7b7372dacdce8b36eb3ad6d410ee8404
Eric Chen <echen9870@gmail.com>
2023-09-14 13:10:20 -0400

    Intial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-purple-400 h-span h-screen">
+        <p>Hi</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
