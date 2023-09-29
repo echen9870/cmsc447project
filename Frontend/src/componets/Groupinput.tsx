b18f57c5e41d042407f05fb0cbcda4325ac10856
Eric Chen <echen9870@gmail.com>
2023-09-29 16:48:14 -0400

    Removed Frontend

--- Frontend/src/componets/Groupinput.tsx
+++
@@ -1,9 +0,0 @@
-const Groupinput = () => {
-  return (
-    <div className="border-solid border-2 rounded-md bg-prismGroupInput border-gray-500 p-4 m-2">
-      <p>Task Input</p>
-    </div>
-  );
-};
-
-export default Groupinput;
--- Frontend/src/componets/Taskinput.tsx
+++
@@ -1,9 +0,0 @@
-const Taskinput = () => {
-  return (
-    <button className="bg-green-500 text-white m-2 py-1 px-2 rounded">
-      Add Task
-    </button>
-  );
-};
-
-export default Taskinput;
