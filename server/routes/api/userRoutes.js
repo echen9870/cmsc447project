5b26d3fcae5e7f8ba8dc6eb8afe8cbd100cac46b
IcramDoku <vt69093@umbc.edu>
2023-10-01 16:01:18 -0400

    Add Hashing to Passwords Without Breaking Code

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,31 +1,76 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../../models/userModel');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/userModel');
+const crypto = require('crypto');
 
-// Route for user registration
+const generateSecretKey = () => {
+  return crypto.randomBytes(64).toString('hex');
+};
+
+//user registration
 router.post('/signup', async (req, res) => {
-  console.log(req.body);
   try {
     const { username, password } = req.body;
-    const user = new User({ username, password });
+
+    //validate input
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+    //check if user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      console.log("Username already exists");
+      return res.status(409).json({ error: 'Username already exists' });
+    }
+
+    //hash password before saving it
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({ username, password: hashedPassword });
     await user.save();
+
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: 'An error occurred' });
   }
 });
 
-// Route for user login (authentication)
+//user login (authentication)
 router.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await User.findOne({ username, password });
-    if (user) {
-      res.status(200).json({ message: 'Login successful' });
+
+    //validate input
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+    const user = await User.findOne({ username });
+
+    //check if user exists
+    if (!user) {
+      console.log("Username does not exist");
+      return res.status(401).json({ error: 'Authentication failed' });
+    }
+
+    // Compare the input password with the hashed password in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      // Generate and return a JWT token for authenticated requests
+      const token = jwt.sign({ userId: user._id }, generateSecretKey(), { expiresIn: '1h' });
+      return res.status(200).json({ message: 'Login successful', token });
     } else {
-      res.status(401).json({ error: 'Authentication failed' });
+      console.log("incorrect password");
+      return res.status(401).json({ error: 'Authentication failed' });
     }
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: 'An error occurred' });
   }
 });
