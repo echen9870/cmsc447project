71fea3377cd9e386ee25c489727ca51702719052
IcramDoku <107223903+IcramDoku@users.noreply.github.com>
2023-10-06 12:37:12 -0400

    Update groupModel.js

--- server/models/groupModel.js
+++ server/models/groupModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  members: { type: [String] }
+  members: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 const Group = mongoose.model('Group', groupSchema);
