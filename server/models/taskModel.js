3d54b3bfa97aa044ea86e6364a5ff4720539620d
IcramDoku <107223903+IcramDoku@users.noreply.github.com>
2023-10-06 12:35:08 -0400

    Update taskModel.js

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  groupId: { type: String, required: true },
+  //taskID is auto-generated aka objectID 
+  groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Group' },//{ type: String, required: true },
   name: { type: String, required: true },
-  description: { type: String, default: "" },
-  assignedUsers: { type: [String] },
-  completed: { type: Boolean, default: false },
+  description: { type: String, default: "" }, //task Created
+  assignedUsers: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, //userID
+  completed: { type: Boolean, default: false }, 
   createdAt: { type: Date, default: Date.now },
   dueAt: { type: Date }
 });
