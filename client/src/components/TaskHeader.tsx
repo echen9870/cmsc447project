bb2dea4fc84f8db81781e72727ee709faec80f46
IcramDoku <vt69093@umbc.edu>
2023-10-23 20:26:02 -0400

    optimize taskheader update date and add placeholders

--- client/src/components/TaskHeader.tsx
+++ client/src/components/TaskHeader.tsx
@@ -11,31 +11,36 @@ interface Props {
   onGroupMembersChange:(groupID:string, updateMember:string, addMember:boolean) => {};
 }
 
-const Taskheader = ({ groupID,isOwner,onGroupDelete,onGroupNameChange,onTaskAdd,onGroupMembersChange }: Props ) => {
+const Taskheader = ({ groupID, isOwner, onGroupDelete, onGroupNameChange, onTaskAdd, onGroupMembersChange }: Props ) => {
 
   const [changeNameView,setChangeNameView] = useState(false);
   const [newName,setNewName] = useState("");
   //new 
   const [addDeleteMember,setAddDeleteMember] = useState(false);
   const [newMember,setNewMemeber] = useState("");
-  const [add, setAdd] = useState(false);
 
   // State to store the actual members from the API
   const [actualMembers, setActualMembers] = useState([]);
 
+  // Define a function to fetch and update the list of group members
+  const updateMembersList = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/group/list_group_members/${groupID}`
+      );
+      if (response.status === 200) {
+        setActualMembers(response.data);
+      } else {
+        console.error("Error: Unexpected response status", response.status);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  // Fetch the list of group members when the component mounts or 'groupID' changes
   useEffect(() => {
-    axios.get(`http://localhost:3000/group/list_group_members/${groupID}`)
-      .then(response => {
-        // Check if the response status is OK (status code 200)
-        if (response.status === 200) {
-          setActualMembers(response.data);
-        } else {
-          console.error('Error: Unexpected response status', response.status);
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+    updateMembersList();
   }, [groupID]);
   
   return (
@@ -56,26 +61,26 @@ const Taskheader = ({ groupID,isOwner,onGroupDelete,onGroupNameChange,onTaskAdd,
         {isOwner && <button className="bg-purple-500  text-white ml-2 m-2 py-1 px-2 rounded" onClick = {() => setChangeNameView(!changeNameView)}>
           Change Name
         </button>}
-        {changeNameView && <input type = "text" className="" onChange = {(e) => setNewName(e.target.value)} value={newName}></input>} 
-        {changeNameView && <button className="bg-purple-500  text-white ml-2 m-2 py-1 px-2 rounded" onClick={() => onGroupNameChange(groupID,newName)} >
+        {changeNameView && <input type = "text" placeholder="Change Group Name" className="" onChange = {(e) => setNewName(e.target.value)} value={newName}></input>} 
+        {changeNameView && <button className="bg-purple-500  text-white ml-2 m-2 py-1 px-2 rounded" onClick={() => {setChangeNameView(!changeNameView); onGroupNameChange(groupID,newName); setNewName("");}} >
           Edit
         </button>}
         {isOwner && <button className="bg-red-500  text-white m-2 py-1 px-2 rounded" onClick = {() => onGroupDelete(groupID)}>
           Delete Group
         </button>}
         {/* allows members changes*/}
-        {isOwner && <button className="bg-yellow-500  text-white ml-2 m-2 py-1 px-2 rounded" onClick = {() => setAddDeleteMember(!addDeleteMember)}>
+        {isOwner && <button className="bg-yellow-500  text-white ml-2 m-2 py-1 px-2 rounded" onClick = {() => {updateMembersList(); setAddDeleteMember(!addDeleteMember);}}>
           Update Members
         </button>}
         {/* will help us add a new member*/}
         {addDeleteMember && <input type="text" placeholder="Add new member" onChange = {(e) => setNewMemeber(e.target.value)} value={newMember}/>}
-        {addDeleteMember &&  <button className="bg-yellow-400  text-white ml-2 m-2 py-1 px-2 rounded" onClick={() => { setAdd(true); onGroupMembersChange(groupID,newMember,add);}} >
+        {addDeleteMember &&  <button className="bg-yellow-400  text-white ml-2 m-2 py-1 px-2 rounded" onClick={() => {setAddDeleteMember(!addDeleteMember); onGroupMembersChange(groupID,newMember,true); setNewMemeber("");}} >
           Add Member
         </button>}
         {/* Display and allow removal of actual members */}
         {addDeleteMember && <select>
           {actualMembers
-            .filter(member => member !== null) // don't display possible empty values
+            .filter(member => member !== null) // Don't display possible empty values
             .map((member, index) => (
               <option key={index} value={member}>
                 {member}
@@ -86,8 +91,8 @@ const Taskheader = ({ groupID,isOwner,onGroupDelete,onGroupNameChange,onTaskAdd,
             const selectElement = document.querySelector('select'); //find element in
             if (selectElement) {
               const selectedMember = selectElement.value;
-              setAdd(false);
-              onGroupMembersChange(groupID, selectedMember, add);
+              setAddDeleteMember(!addDeleteMember);
+              onGroupMembersChange(groupID, selectedMember, false);
             }
           }}>
           Remove
